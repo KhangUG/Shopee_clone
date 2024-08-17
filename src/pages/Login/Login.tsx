9eafdc12421f490c05d51d58eeda88a9e2a3516c
Khangug <giapvankhang6789@gmail.com>
2024-08-17 21:52:31 +0700

    sua loi tac gia

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -36,7 +36,7 @@ export default function Register() {
               <div className='flex item-center justify-center mt-8'>
                 <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
                 <Link className='text-red-400 ml-1' to='/register'>
-                  Đăng ký
+                  Đăng ký hehe
                 </Link>
               </div>
             </form>
