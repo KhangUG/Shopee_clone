88657d33eca88018fbce1cb3ef80a2a6f66d3cea
Khangug <giapvankhang6789@gmail.com>
2024-08-19 16:27:04 +0700

    Custom class .container bang plugin Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
         orange: '#ee4d2d'
+      },
+      maxWidth: {
+        '7xl': '80rem' // Hoặc bất kỳ giá trị nào bạn muốn
       }
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('maxWidth.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+  ]
 }
